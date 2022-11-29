0e79f61b2d6a4d105a4485849e1e99cc2d94eb1e
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-29 09:24:17 +0530

    passenger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPassengerComponent } from './view-passenger/view-passenger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPassengerComponent
   ],
   imports: [
     BrowserModule,
